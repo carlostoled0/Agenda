1b173919cf4e8ae7b244e5a1e30f808b9a55c311
carlostoled0 <acmt.uxx@gmail.com>
2025-05-09 00:19:12 -0300

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-const CLIENT_ID = process.env.CLIENT_ID;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-
-export default function handler(req, res) {
-  const scope = 'https://www.googleapis.com/auth/youtube.readonly';
-  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
-  res.writeHead(302, { Location: authUrl });
-  res.end();
-}
